3e7873d344aad757ee5cb35588a17f9fb095d1e2
AnneLaure Le <annelaurele@hotmail.fr>
2022-01-20 19:03:08 +0100

    yelp MVP

---
+++ app/controllers/reviews_controller.rb
@@ -0,0 +1,29 @@
+class ReviewsController < ApplicationController
+  def index
+    @reviews = Review.where(restaurant_id: params[:restaurant_id])
+  end
+
+  def show
+    @reviews = Review.where(restaurant_id: @restaurant)
+  end
+
+  def new
+    @restaurant = Restaurant.find(params[:restaurant_id])
+    @review = Review.new
+  end
+
+  def create
+    @review = Review.new(review_params)
+    # we need `restaurant_id` to associate review with corresponding restaurant
+    @restaurant = Restaurant.find(params[:restaurant_id])
+    @review.restaurant = @restaurant
+    @review.save
+    redirect_to restaurant_path(@restaurant)
+  end
+
+  private
+
+  def review_params
+    params.require(:review).permit(:content, :rating)
+  end
+end
